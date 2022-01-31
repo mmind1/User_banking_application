7ffc791e8acf85e1788d3cfdb3d323be8ebe71dd
mmind1 <mrm351@rutgers.edu>
2022-01-31 13:02:03 -0500

    Second commit

--- FirstBank/src/FirstBank.java
+++ FirstBank/src/FirstBank.java
@@ -3,7 +3,7 @@ public class FirstBank {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Account user = new Account("First Name Last Name" , "A00001");
+		Account user = new Account("Matt Mindur" , "A00001");
 		user.showMenu();
 		
 		// Make a new user here
